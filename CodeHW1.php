1149b5ceea2cc90aa4f3b419763d5c7055240891
Ruojing <apple@appletekiMacBook-Air.local>
2016-03-01 15:08:15 -0500

    Completed HW1
    2 Challenges

--- CodeHW1.php
+++ CodeHW1.php
@@ -3,10 +3,13 @@
 <head> 
 <meta charset="UTF-8">
 </head> 
-
-<h1>Challenge: Correct Change</h1>
 <body> 
 <?php
+
+/* Challenge #1
+*/
+
+echo "<h1>Challenge: Correct Change</h1>";
 
 $change = 246;
 
@@ -28,6 +31,35 @@ echo "<br>";
 
 echo "Your change is: $dollar dollar(s), $quarter quarter(s), $dime dime(s), $nickel nickel(s), $cent cent(s)";
 
+/* Challenge #2
+*/
+
+echo "<h1>Challenge: 99 Bottles of Beer</h1>";
+
+for ($count = 99; $count >= 1; --$count)
+{
+	echo "$count bottles of beer on the wall, $count bottles of beer!";
+	echo "<br>";
+	$count2 = $count - 1;
+	echo "Take one down, pass it around, $count2 bottles of beer on the wall.";
+	echo "<br>";
+}
+
+/* Challenge #2a
+*/
+
+echo "<h1>Challenge: 4 Bottles of Beer</h1>";
+
+$input = 4;
+
+for ($count = $input; $count >= 1; --$count)
+{
+	echo "$count bottles of beer on the wall, $count bottles of beer!";
+	echo "<br>";
+	$count2 = $count - 1;
+	echo "Take one down, pass it around, $count2 bottles of beer on the wall.";
+	echo "<br>";
+}
 
 
 ?>
